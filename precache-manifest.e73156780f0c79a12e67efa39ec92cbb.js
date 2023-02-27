7e9c4e21ab0bf53735ea8cea728ca42d82859c64
thanhnq112 <thanhbu1998@gmail.com>
2023-02-27 22:21:42 +0300

    Updates

---
+++ precache-manifest.e73156780f0c79a12e67efa39ec92cbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d71a8ba7ef7e84857e03",
+    "url": "/thanhnguyen/static/css/main.4e32ad22.chunk.css"
+  },
+  {
+    "revision": "d71a8ba7ef7e84857e03",
+    "url": "/thanhnguyen/static/js/main.d71a8ba7.chunk.js"
+  },
+  {
+    "revision": "9217d35307c50c8d924d",
+    "url": "/thanhnguyen/static/js/1.9217d353.chunk.js"
+  },
+  {
+    "revision": "2be6ecf0f37222cf3e5e",
+    "url": "/thanhnguyen/static/css/2.92c9c0ed.chunk.css"
+  },
+  {
+    "revision": "2be6ecf0f37222cf3e5e",
+    "url": "/thanhnguyen/static/js/2.2be6ecf0.chunk.js"
+  },
+  {
+    "revision": "8d09f419f97450531180",
+    "url": "/thanhnguyen/static/js/runtime~main.8d09f419.js"
+  },
+  {
+    "revision": "76d34bce8488e5795d5ff7661e3088eb",
+    "url": "/thanhnguyen/index.html"
+  }
+];
